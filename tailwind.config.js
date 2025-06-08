c4f84a874bbdda4fecdee3ff06879454b1ff2773
RIZKY JUNI ARIGAYO <rizkyjuni.aa@gmail.com>
2025-06-08 05:33:24 +0000

    Restored to '7a41db2aa75af0d40c12499edd923cf05662aba5'
    Replit-Restored-To: 7a41db2aa75af0d40c12499edd923cf05662aba5

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./static/**/*.js"],
-  darkMode: 'class',
+  content: [
+    "./templates/**/*.html",
+    "./static/**/*.js"
+  ],
   theme: {
     extend: {},
   },
